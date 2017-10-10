5cbf21fae7de08650817e6d3ec2ed002c046be35
Candy Tsai <candy02058912@gmail.com>
2017-10-10 16:51:38 +0800

    Fix server address settings

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -111,6 +111,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
-    server_address = (port, 8000)
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
